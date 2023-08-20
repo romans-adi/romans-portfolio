1ac073ea7f29bc8cf9f04f5f5a1ee8896049b109
Romans S <romans.spilaks@gmail.com>
2023-08-21 02:10:32 +0300

    Fix typos and minor issues while lintering project

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -2,45 +2,47 @@ import { Card, CardContent, Typography } from '@mui/material';
 import { PropTypes } from 'prop-types';
 import './ProjectCard.scss';
 
-const ProjectCard = ({ title, description, technologies, date, links }) => {
-  return (
-    <Card className="project-card">
-      <CardContent className='project-content'>
-        <Typography variant="h6" className="project-title">
-          {title}
-        </Typography>
-        <Typography variant="body1" className="project-description">
-          {description}
-        </Typography>
-        <div className="project-footer">
+const ProjectCard = (
+  {
+    title, description, technologies, date, links,
+  },
+) => (
+  <Card className="project-card">
+    <CardContent className="project-content">
+      <Typography variant="h6" className="project-title">
+        {title}
+      </Typography>
+      <Typography variant="body1" className="project-description">
+        {description}
+      </Typography>
+      <div className="project-footer">
         <div className="technologies">
-          {technologies.map((tech, index) => (
-            <span key={index} className="badge technology-badge">
+          {technologies.map((tech) => (
+            <span key={tech} className="badge technology-badge">
               {tech}
             </span>
           ))}
         </div>
         <div className="links">
-          {links.map((link, index) => (
-    <a
-    key={index}
-    href={link.url}
-    target="_blank"
-    rel="noopener noreferrer"
-    className="badge link-badge"
-  >
-    {link.text}
-  </a>
+          {links.map((link) => (
+            <a
+              key={link.url}
+              href={link.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="badge link-badge"
+            >
+              {link.text}
+            </a>
           ))}
         </div>
-        </div>
-        <Typography variant="subtitle2" className="project-date">
-    {date}
-  </Typography>
-      </CardContent>
-    </Card>
-  );
-};
+      </div>
+      <Typography variant="subtitle2" className="project-date">
+        {date}
+      </Typography>
+    </CardContent>
+  </Card>
+);
 
 ProjectCard.propTypes = {
   title: PropTypes.string.isRequired,
@@ -50,7 +52,7 @@ ProjectCard.propTypes = {
   links: PropTypes.arrayOf(
     PropTypes.shape({
       text: PropTypes.string.isRequired,
-    })
+    }),
   ).isRequired,
 };
 
